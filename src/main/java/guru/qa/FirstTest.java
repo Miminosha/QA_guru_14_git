ad8944053777909d1e594233d4f2366fd315347e
Miminosha <anna.memikova@yandex.ru>
2022-07-29 14:39:15 +0300

    Feature update

--- src/main/java/guru/qa/FirstTest.java
+++ src/main/java/guru/qa/FirstTest.java
@@ -2,11 +2,13 @@ package guru.qa;
 
 public class FirstTest {
     public static void main (String[] args){
-        int num1 = 3, num2 = 4;
-        if (num1 ==num2){
+        int num1 = 5, num2 = 4;
+        if (num1 == num2){
             System.out.println("У вас равенство");
+        } else if (num1 > num2){
+            System.out.println("У вас неравенство, первое число больше второго");
         } else {
-            System.out.println("У вас неравенство");
+            System.out.println("У вас неравенство, второе число больше первого");
         }
     }
 }
